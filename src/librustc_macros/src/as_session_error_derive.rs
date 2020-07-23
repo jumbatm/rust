950688c1d26e2cf1a32807f8f761a1d7f5e3fb8c
jumbatm <30644300+jumbatm@users.noreply.github.com>
2020-07-23 17:32:09 +1000

    Start supporting specifying lint id.

--- src/librustc_macros/src/as_session_error_derive.rs
+++ src/librustc_macros/src/as_session_error_derive.rs
@@ -1,8 +1,8 @@
-#![allow(unused)] // FIXME: Remove
 use quote::format_ident;
 use quote::quote;
 
-use std::collections::{HashMap, HashSet};
+
+use std::collections::HashMap;
 
 /// Implements #[derive(AsSessionError)], which allows for errors to be specified as a struct, independent
 /// from the actual diagnostics emitting code.
@@ -32,31 +32,19 @@ use std::collections::{HashMap, HashSet};
 /// });
 /// ```
 pub fn as_session_error_derive(s: synstructure::Structure<'_>) -> proc_macro2::TokenStream {
-    let ast = &s.ast();
-    let attrs = &ast.attrs;
-    let get_meta_value = |attrs: &[syn::Attribute], key: &str| {
-        if let syn::Meta::NameValue(syn::MetaNameValue { lit: syn::Lit::Str(s), .. }) = attrs
-            .iter()
-            .find(|&attr| attr.path.segments.first().unwrap().ident == key)?
-            .parse_meta()
-            .expect("Could not parse attribute contents")
-        {
-            Some(s.value())
-        } else {
-            None
-        }
-    };
+    // Names for the diagnostic we build and the session we build it from.
+    let diag = format_ident!("diag");
+    let sess = format_ident!("sess");
 
+    // Convenience bindings.
+    let ast = s.ast();
+    let attrs = &ast.attrs;
     let fields: &syn::Fields = if let syn::Data::Struct(syn::DataStruct { fields, .. }) = &ast.data
     {
-        Some(fields)
+       fields
     } else {
-        None
-    }
-    .unwrap();
-
-    // The name of the diagnostic builder we'll be working with.
-    let diag = format_ident!("diag");
+        todo!()
+    };
 
     let mut builder = SessionDeriveBuilder::new(&diag, fields);
 
@@ -76,12 +64,10 @@ pub fn as_session_error_derive(s: synstructure::Structure<'_>) -> proc_macro2::T
         let result = field.attrs.iter().map(|attr| {
             builder
                 .generate_field_code(
-                    &field_binding.binding,
                     attr,
                     FieldInfo {
                         vis: &field.vis,
                         binding: field_binding,
-                        ident: field.ident.as_ref(),
                         ty: &field.ty,
                     },
                 )
@@ -91,31 +77,44 @@ pub fn as_session_error_derive(s: synstructure::Structure<'_>) -> proc_macro2::T
             #(#result);*
         };
     });
+    // Finally, put it all together.
+    let implementation = match builder.kind {
+        None => Err(SessionDeriveBuilderError::IdNotProvided),
+        Some(kind) => Ok(match kind {
+            DiagnosticId::Lint(_tokens) => todo!(),
+            DiagnosticId::Error(code) => {
+                quote! {
+                    let mut #diag = #sess.struct_err_with_code("", rustc_errors::DiagnosticId::Error(#code));
+                    #(#preamble)*;
+                    match self {
+                        #body
+                    }
+                    #diag
+                }
+            }
+        }),
+    }
+    .unwrap_or_else(|_| todo!("Proper error handling"));
 
-    // FIXME: Replace this with gen_impl.
-    // FIXME: Want to be able to specify a Lint DiagnosticId too.
     s.gen_impl(quote! {
         gen impl<'a> rustc_errors::AsError<'a> for @Self {
             type Session = rustc_session::Session;
-            fn as_error(self, sess: &'a Self::Session) -> rustc_errors::DiagnosticBuilder {
-                let mut #diag = sess.struct_err("");
-                #(#preamble)*;
-                match self {
-                    #body
-                }
-                #diag
+            fn as_error(self, #sess: &'a Self::Session) -> rustc_errors::DiagnosticBuilder {
+                #implementation
             }
         }
     })
 }
 
+// FIXME: Remove unused fields.
+#[allow(unused)]
 struct FieldInfo<'a> {
     vis: &'a syn::Visibility,
-    ident: Option<&'a syn::Ident>,
     binding: &'a synstructure::BindingInfo<'a>,
     ty: &'a syn::Type,
 }
 
+#[allow(unused)]
 struct VariantInfo<'a> {
     ident: &'a syn::Ident,
 }
@@ -138,25 +137,53 @@ struct SessionDeriveBuilder<'a> {
     /// Store a map of field name to its corresponding field. This is built on construction of the
     /// derive builder.
     fields: HashMap<String, &'a syn::Field>,
+
+    /// The identifier to use for the generated DiagnosticBuilder instance.
     diag: &'a syn::Ident,
+
+    /// Whether this is a lint or an error. This dictates how the diag will be initialised.
+    kind: Option<DiagnosticId>,
 }
 
+#[allow(unused)]
+enum DiagnosticId {
+    Error(proc_macro2::TokenStream),
+    Lint(proc_macro2::TokenStream),
+}
+
+#[derive(Debug)]
+enum SessionDeriveBuilderError {
+    SynError(syn::Error),
+    IdNotProvided,
+    IdMultiplyProvided,
+}
+
+impl std::convert::From<syn::Error> for SessionDeriveBuilderError {
+    fn from(e: syn::Error) -> Self {
+        SessionDeriveBuilderError::SynError(e)
+    }
+}
+
+#[deny(unused_must_use)]
 impl<'a> SessionDeriveBuilder<'a> {
     fn new(diag: &'a syn::Ident, fields: &'a syn::Fields) -> Self {
+        // Build the mapping of field names to fields. This allows attributes to peek values from
+        // other fields.
         let mut fields_map = HashMap::new();
         for field in fields.iter() {
             if let Some(ident) = &field.ident {
                 fields_map.insert(ident.to_string(), field);
             }
         }
-        Self { diag, fields: fields_map }
+
+        Self { diag, fields: fields_map, kind: None, }
     }
 
     fn generate_structure_code(
         &mut self,
         attr: &syn::Attribute,
-        info: VariantInfo<'a>,
-    ) -> syn::Result<proc_macro2::TokenStream> {
+        _info: VariantInfo<'a>, // FIXME: Remove this parameter?
+    ) -> Result<proc_macro2::TokenStream, SessionDeriveBuilderError> {
         let diag = self.diag;
         Ok(match attr.parse_meta()? {
             syn::Meta::NameValue(syn::MetaNameValue { lit: syn::Lit::Str(s), .. }) => {
@@ -170,11 +197,68 @@ impl<'a> SessionDeriveBuilder<'a> {
                         }
                     }
                     "code" => {
-                        quote! {
-                            #diag.code(rustc_errors::DiagnosticId::Error(#formatted_str));
+                        self.set_kind_once(DiagnosticId::Error(formatted_str))?;
+                        // This attribute is only allowed to be applied once, and the attribute
+                        // will be set in the initialisation code.
+                        quote! {}
+                    }
+                    "lint" => todo!(),
+                    _ => unimplemented!(),
+                }
+            }
+            _ => todo!("unhandled meta kind"),
+        })
+    }
+
+    #[must_use]
+    fn set_kind_once(&mut self, kind: DiagnosticId) -> Result<(), SessionDeriveBuilderError> {
+        if self.kind.is_none() {
+            self.kind = Some(kind);
+            Ok(())
+        } else {
+            Err(SessionDeriveBuilderError::IdMultiplyProvided)
+        }
+    }
+
+    fn generate_field_code(
+        &mut self,
+        attr: &syn::Attribute,
+        info: FieldInfo<'_>,
+    ) -> Result<proc_macro2::TokenStream, SessionDeriveBuilderError> {
+        let diag = self.diag;
+        let field_binding = &info.binding.binding;
+        let name = attr.path.segments.last().unwrap().ident.to_string();
+        let name = name.as_str();
+        // At this point, we need to dispatch based on the attribute key + the
+        // type.
+        let meta = attr.parse_meta()?;
+        let field_ty_path = if let syn::Type::Path(path) = info.ty { path } else { todo!() };
+        Ok(match meta {
+            syn::Meta::NameValue(syn::MetaNameValue { lit: syn::Lit::Str(s), .. }) => {
+                let formatted_str = self.build_format(&s.value());
+                match name {
+                    "error" => {
+                        if type_matches(field_ty_path, &["rustc_span", "Span"]) {
+                            quote! {
+                                #diag.set_span(*#field_binding);
+                                #diag.set_primary_message(#formatted_str);
+                            }
+                        } else {
+                            quote! {
+                                #diag.set_primary_message(#formatted_str);
+                            }
                         }
                     }
-                    _ => unimplemented!(),
+                    "label" => {
+                        if type_matches(field_ty_path, &["rustc_span", "Span"]) {
+                            quote! {
+                                #diag.span_label(*#field_binding, #formatted_str);
+                            }
+                        } else {
+                            todo!("Error: Label applied to non-Span field ({:?})", field_ty_path)
+                        }
+                    }
+                    other => todo!("Unrecognised field: {}", other),
                 }
             }
             _ => todo!("unhandled meta kind"),
@@ -199,7 +283,7 @@ impl<'a> SessionDeriveBuilder<'a> {
     fn build_format(&self, input: &String) -> proc_macro2::TokenStream {
         // Keep track of which fields have been referenced. First, initialise all to false:
         let mut field_referenced: HashMap<String, bool> = HashMap::new();
-        for field_name in self.fields.iter().map(|(k, v)| k) {
+        for field_name in self.fields.iter().map(|(k, _)| k) {
             field_referenced.insert(field_name.clone(), false);
         }
 
@@ -257,50 +341,5 @@ impl<'a> SessionDeriveBuilder<'a> {
         quote! {
         format!(#input #(,#args)*)
         }
-    }
-
-    fn generate_field_code(
-        &mut self,
-        field_binding: &syn::Ident,
-        attr: &syn::Attribute,
-        info: FieldInfo<'_>,
-    ) -> syn::Result<proc_macro2::TokenStream> {
-        let diag = &self.diag;
-        let name = attr.path.segments.last().unwrap().ident.to_string();
-        let name = name.as_str();
-        // At this point, we need to dispatch based on the attribute key + the
-        // type.
-        let meta = attr.parse_meta()?;
-        let field_ty_path = if let syn::Type::Path(path) = info.ty { path } else { todo!() };
-        Ok(match meta {
-            syn::Meta::NameValue(syn::MetaNameValue { lit: syn::Lit::Str(s), .. }) => {
-                let formatted_str = self.build_format(&s.value());
-                match name {
-                    "error" => {
-                        if type_matches(field_ty_path, &["rustc_span", "Span"]) {
-                            quote! {
-                                #diag.set_span(*#field_binding);
-                                #diag.set_primary_message(#formatted_str);
-                            }
-                        } else {
-                            quote! {
-                                #diag.set_primary_message(#formatted_str);
-                            }
-                        }
-                    }
-                    "label" => {
-                        if type_matches(field_ty_path, &["rustc_span", "Span"]) {
-                            quote! {
-                                #diag.span_label(*#field_binding, #formatted_str);
-                            }
-                        } else {
-                            todo!("Error: Label applied to non-Span field ({:?})", field_ty_path)
-                        }
-                    }
-                    other => todo!("Unrecognised field: {}", other),
-                }
-            }
-            _ => todo!("unhandled meta kind"),
-        })
     }
 }
